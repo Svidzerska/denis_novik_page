98b3c9dc823d392ab5959a2732db6a2b2fd46090
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-03-16 17:43:49 +0200

    mobile version for headers was done without styles

--- src/components/headers/Headers.tsx
+++ src/components/headers/Headers.tsx
@@ -1,6 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import useMediaQuery from "../elements/hooks/useMediaQuery";
 import Button from "../elements/Button";
 import foto from '../../images/Group_2_1.png';
+import menu from '../../images/menu.png';
+import foto_small from '../../images/Group_2_1_small.png';
+import menu_close from '../../images/menu_close.png';
+
 import './headers.scss';
 import { setLanguage } from "../../app/features/languageSlice";
 import { useDispatch, useSelector, RootStateOrAny } from "react-redux";
@@ -12,11 +17,15 @@ function Headers(props: any) {
 
    const language = useSelector((state: RootStateOrAny) => state.language.selectLanguage);
    const section = useSelector((state: RootStateOrAny) => state.navigation.selectSection);
+   const matches = useMediaQuery('(max-width: 640px)');
+
+   const [menuText, setMenuText] = useState(false);
+
 
 
    useEffect(() => {
-      console.log(section);
-   }, [section]);
+      console.log(matches);
+   }, [matches]);
 
    const info = () => {
       if (language === "Eng") {
@@ -43,29 +52,32 @@ function Headers(props: any) {
    }
 
    const handleButtonEng = (e: any) => {
+      setMenuText(false);
       dispatch(setSection("0"));
       dispatch(setLanguage("Eng"));
    }
 
    const handleButtonUkr = (e: any) => {
+      setMenuText(false);
       dispatch(setSection("0"));
       dispatch(setLanguage("Ukr"));
    }
 
    const handleNavigation = (e: any) => {
+      setMenuText(false);
       console.log(e.target.id);
       dispatch(setSection(e.target.id));
    }
 
 
-   const headerNavigationList = info()?.headersNavigator.map((element,index) => {
+   const headerNavigationList = info()?.headersNavigator.map((element, index) => {
       const indexSection = index.toString();
       return (
          <p key={element} >
             <a href={'#' + index + 'Section'}
-             id={indexSection}
-              onClick={handleNavigation}
-              className={section === indexSection ? "navigations_items__active" : "navigations_items"}>
+               id={indexSection}
+               onClick={handleNavigation}
+               className={section === indexSection ? "navigations_items__active" : "navigations_items"}>
                {element}
             </a>
          </p>
@@ -84,9 +96,9 @@ function Headers(props: any) {
       return (
          <div id="0Section" className="headers__info_name">
             <div className="headers__name">
-                  <span>{info()?.name}</span>
-                  <br />
-                  <span>{info()?.surname}</span>
+               <span>{info()?.name}</span>
+               <br />
+               <span>{info()?.surname}</span>
             </div>
             <div className="headers__info">
                <p>{info()?.info_profession}</p>
@@ -105,13 +117,70 @@ function Headers(props: any) {
       )
    }
 
-   return (<div className="headers">
-      {renderNavigation()}
-      {renderHeadersInfoName()}
-      <div className="headers__picture">
-         <img src={foto} alt="Novik" />
-      </div>
-   </div>
+   const handleMenu = (e:any) => {
+      setMenuText(true);
+   }
+
+   const renderHeadersSmall = () => {
+      return (<div className="headers_small">
+         <div>
+            <div className="headers__name">
+               <span>{info()?.name}</span>
+               <br />
+               <span>{info()?.surname}</span>
+            </div>
+            <div className="headers__info">
+               <p>{info()?.info_profession}</p>
+               <p>{info()?.info_age}</p>
+            </div>
+         </div>
+         <div>
+            <Button value={<img src={menu}></img>} onClick={handleMenu}/>
+         </div>
+         <div><img src={foto_small} alt="fotoman" /></div>
+      </div>)
+   }
+
+   const handleClosePress = () => {
+      setMenuText(false);
+   }
+
+   const renderMenuSmall = () => {
+      return (
+         <div>
+            <div>
+               {renderNavigation()}
+               <Button value={<img src={menu_close} alt=""/>} onClick={handleClosePress}/>
+               <div className="headers__buttonsLang">
+               <Button onClick={handleButtonEng}
+                  value={info()?.buttonLang2}
+                  className={language === "Eng" ? "buttonLang__active" : "buttonLang"}
+               /> | <Button onClick={handleButtonUkr}
+                  value={info()?.buttonLang1}
+                  className={language === "Ukr" ? "buttonLang__active" : "buttonLang"}
+               />
+            </div>
+            </div>
+
+         </div>
+      )
+   }
+
+   return (
+      matches ?
+         <div>{menuText ? renderMenuSmall() : renderHeadersSmall()}</div>
+         :
+         (
+            <div className="headers">
+               <div>
+                  {`The view port is ${matches ? 'less than' : 'more than'} 640px pixels wide`}
+               </div>
+               {renderNavigation()}
+               {renderHeadersInfoName()}
+               <div className="headers__picture">
+                  <img src={foto} alt="Novik" />
+               </div>
+            </div>)
    )
 }
 
