01cde0d2f88ea704bca293764c7812dafbcf93b2
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-03-17 11:24:59 +0200

    router try

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,36 @@ import Info from './components/information/Info';
 import Skills from './components/skills/Skills';
 import Portfolio from './components/portfolio/Portfolio';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, RouterProps, Route, RouteProps, Routes, Outlet} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Headers/>
-      <Info/>
-      <Skills/>
-      <Portfolio/>
-      <Footer/>
-    </div>
+     <BrowserRouter>
+        <div className="App">
+           <Routes>
+              <Route path='/' element={<Layout />}>
+                 <Route path='/' element ={<Headers/>} />
+                 <Route path='/headers' element ={<Headers/>} />
+                 <Route path='/info' element ={<Info/>} />
+                 <Route path='/skills' element ={<Skills/>} />
+                 <Route path='/portfolio' element ={<Portfolio/>} />
+                 <Route path='/footer' element ={<Footer/>} />
+              </Route>
+           </Routes>
+        </div>
+      </BrowserRouter>
   );
+}
+
+
+function Layout() {
+   return (
+     <div>
+       <main>
+         <Outlet />
+       </main>
+     </div>
+   );
 }
 
 export default App;
